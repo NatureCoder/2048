9cc8684fa03a7844424cfee91943709d30ab3dca
NatureCoder <zremco@gmail.com>
2018-03-02 23:34:55 +0100

    - fixed missing require webpack - fixed tests not working

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,10 @@
 import 'mocha';
 import { expect } from 'chai';
 
-
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      const result = [1,2,3].indexOf(4);
+      const result = [1, 2, 3].indexOf(4);
       expect(result).to.equal(-1);
     });
   });
